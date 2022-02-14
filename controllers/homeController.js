7a06a98d50a011457e355d1fabb95e8819a0b0b3
JAYESH8011 <jayeshkalra2002@gmail.com>
2022-02-14 09:27:19 +0530

    minor changes

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,5 +2,5 @@ const CustomError = require("../utils/customError")
 const bigPromise = require("../middlewares/bigPromise")
 
 exports.home = bigPromise((req, res, next) => {
-    res.send("<h2>this is the Ecoom app under contruction</h2>")
+    res.send("<h2>Ecoom app backend api</h2>")
 })
